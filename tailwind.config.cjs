1f88286f76ff66913bbaf85ae4435f3fda97c476
Jakub Cichon <jakub@uconn.edu>
2023-02-24 18:41:49 -0500

    initial commit (boilerplate/setup)

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+			fontFamily: {
+				'noto-sans': ['Noto Sans', 'sans-serif'],
+			},
+		},
+  },
+  plugins: [],
+}
