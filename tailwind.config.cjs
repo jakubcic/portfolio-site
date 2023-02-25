b231b2d99c765e59f33b7494faceef681edfc664
Jakub Cichon <jakub@uconn.edu>
2023-02-25 15:34:32 -0500

    formatting, set up App

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
 			fontFamily: {
 				'noto-sans': ['Noto Sans', 'sans-serif'],
 			},
 		},
-  },
-  plugins: [],
-}
+	},
+	plugins: [],
+};
